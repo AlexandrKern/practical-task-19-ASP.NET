ac9cc4d17707ce8e05051368665e4c1ab9d7201c
AlexandrKern <137929102+AlexandrKern@users.noreply.github.com>
2023-11-19 20:29:20 +0300

    Добавьте файлы проекта.

---
+++ practical task 19 ASP.NET/Controllers/ContactsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using practical_task_19_ASP.NET.Models;
+
+namespace practical_task_19_ASP.NET.Controllers
+{
+	public class ContactsController : Controller
+	{
+		private List<Contact> contacts;
+		public ContactsController()
+		{
+			contacts = new List<Contact>();
+			for (int i = 0; i < 10; i++)
+			{
+				contacts.Add(new Contact(i, $"Имя_{i}",
+					$"Отчество_{i}",
+					$"Фамилия_{i}",
+					8900 + i,
+					$"Адрес_{i}",
+					$"Описание_{i}"));
+			}
+		}
+		public IActionResult Index()
+		{
+			return View(contacts);
+		}
+		public IActionResult Details(int id)
+		{
+			Contact contact = contacts.FirstOrDefault(c => c.ID == id);
+			if (contacts == null) return NotFound();
+			return View(contact);
+		}
+	}
+}
+
---
+++ practical task 19 ASP.NET/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿namespace practical_task_19_ASP.NET.Models
+{
+	public class Contact
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string MiddleName { get; set; }
+		public string LastName { get; set; }
+		public int PhoneNumber { get; set; }
+		public string Address { get; set; }
+		public string Description { get; set; }
+		public Contact(int ID, string Name, string MiddleName, string LastName, int PhoneNumber, string Address, string Description)
+		{
+			this.ID = ID;
+			this.Name = Name;
+			this.MiddleName = MiddleName;
+			this.LastName = LastName;
+			this.PhoneNumber = PhoneNumber;
+			this.Address = Address;
+			this.Description = Description;
+		}
+	}
+}
